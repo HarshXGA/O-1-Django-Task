87f7d7a3be8d3a0a51a01360570546683207e922
HarshXGA <harshmiscrits@gamil.com>
2023-09-02 13:56:32 +0530

    Initial commit

---
+++ urlShortner/myapp/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path,include
+
+from . import views
+urlpatterns= [
+    path('task',views.task_page),
+    path('hello', views.hello_world),
+    path('', views.home_page)
+    ]
+
